94c1a20f6ae7a3ac959d587bf2baf1ba5eb753e0
santoshlathika <126945544+santoshlathika@users.noreply.github.com>
2023-03-23 12:37:38 +0530

    Add files via upload

---
+++ Drop_Down_Menu.js
@@ -0,0 +1,11 @@
+/**
+  Custom Dropdown
+  
+  # What? #
+  Part of my "Responsive Menu Concepts" article on CSS-Tricks
+  https://css-tricks.com/responsive-menu-concepts
+
+
+  # 2012 by Tim Pietrusky
+  # timpietrusky.com
+**/
---
+++ custom.js
@@ -0,0 +1,17 @@
+
+						
+		//CSS
+		
+$('ul').css('list-style', 'none');
+$('.D').css('display', 'flex');
+$('.HC').css('justify-content', 'center');
+$('.HSA').css('justify-content','space-around');
+$('.HSB').css('justify-content','space-between');
+$('.T-C').css('text-align','center');
+$('.FW').css('flex-wrap','wrap');
+$('.VC').css('align-items','center');
+$('.VE').css('align-items','end');
+$('.C-P').css('cursor','pointer');
+$('.center').css({"width": "90%","margin": "0 auto"});
+
+			
